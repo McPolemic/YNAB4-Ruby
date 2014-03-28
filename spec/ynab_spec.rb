324a0ac2cf55b4b0083791802be831b3d3afc8fc
Adam Lukens <alukens@purduefed.com>
2014-03-28 18:50:32 -0400

    Clean up some specs

--- spec/ynab_spec.rb
+++ spec/ynab_spec.rb
@@ -4,8 +4,6 @@ describe Ynab do
   describe '#open' do
     let(:ynab) { Ynab.open('./spec/fixtures/Test~E8570C74.ynab4') }
 
-    it 'accepts a file'
-    
     it 'returns an object with transactions' do
       transaction = ynab.transactions.first
       pending "Figure out transactions first"
@@ -18,33 +16,41 @@ describe Ynab do
   describe Ynab::Budget do
     let(:budget) { Ynab::Budget.new }
 
-    it 'adds accounts' do
-      account = 'Savings'
-      budget.add_account account
-      expect(budget.accounts.count).to eq 1
+    describe '#add_account' do
+      it 'adds and stores accounts' do
+        account = 'Savings'
+        budget.add_account account
+        expect(budget.accounts.count).to eq 1
+      end
     end
 
-    it 'adds categories' do
-      category = 'Spending Money'
-      budget.add_category category
-      expect(budget.categories.count).to eq 1
+    describe '#add_category' do
+      it 'adds and stores categories' do
+        category = 'Spending Money'
+        budget.add_category category
+        expect(budget.categories.count).to eq 1
+      end
     end
 
-    it 'adds payees' do
-      payee = 'Target'
-      budget.add_payee(payee)
-      expect(budget.payees.count).to eq 1
+    describe '#add_payee' do
+      it 'adds and stores payees' do
+        payee = 'Target'
+        budget.add_payee(payee)
+        expect(budget.payees.count).to eq 1
+      end
     end
 
-    it 'adds transactions' do
-      t = Ynab::Transaction.new('Savings',
-                                Date.new(2014, 3, 26), 
-                                'Target',
-                                'Spending Money',
-                                "This is a memo",
-                                -100.00)
-      budget.add_transaction(t)
-      expect(budget.transactions.count).to eq 1
+    describe '#add_transaction' do
+      it 'adds and stores transactions' do
+        t = Ynab::Transaction.new('Savings',
+                                  Date.new(2014, 3, 26), 
+                                  'Target',
+                                  'Spending Money',
+                                  "This is a memo",
+                                  -100.00)
+        budget.add_transaction(t)
+        expect(budget.transactions.count).to eq 1
+      end
     end
   end
 
